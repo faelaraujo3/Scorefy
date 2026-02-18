994937bee719ec0babb3ab8e0842a0e0ffb2adc7
Gustavo <gustavo.fffelipe@hotmail.com>
2026-02-17 23:16:05 -0300

    Atualiza povoamento de artistas e albuns

--- server/povoar.py
+++ server/povoar.py
@@ -16,103 +16,178 @@ db.artists.insert_many([
     {
         "id_artista": 1,
         "name": "Linkin Park",
-        "bio": "Banda de Nu Metal americana formada em Califórnia"
+        "bio": "Banda de Nu Metal americana formada em California",
+        "genre": "Nu Metal",
+        "country": "EUA",
+        "formed_year": 1996,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 2,
         "name": "Billie Eilish",
-        "bio": "Cantora e compositora de pop alternativo norte americana"
+        "bio": "Cantora e compositora de pop alternativo norte americana",
+        "genre": "Pop",
+        "country": "EUA",
+        "formed_year": 2015,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 3,
         "name": "Taylor Swift",
-        "bio": "Cantora e compositora americana conhecida por misturar pop e country com letras autobiográficas"
+        "bio": "Cantora e compositora americana conhecida por misturar pop e country com letras autobiograficas",
+        "genre": "Pop",
+        "country": "EUA",
+        "formed_year": 2006,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 4,
         "name": "The Beatles",
-        "bio": "Banda britânica de rock considerada uma das mais influentes da história da música"
+        "bio": "Banda britanica de rock considerada uma das mais influentes da historia da musica",
+        "genre": "Rock",
+        "country": "Reino Unido",
+        "formed_year": 1960,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 5,
         "name": "Nirvana",
-        "bio": "Banda de grunge formada em Seattle que marcou os anos 90"
+        "bio": "Banda de grunge formada em Seattle que marcou os anos 90",
+        "genre": "Grunge",
+        "country": "EUA",
+        "formed_year": 1987,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 6,
         "name": "Legiao Urbana",
-        "bio": "Banda brasileira de rock formada em Brasília com letras marcantes e políticas"
+        "bio": "Banda brasileira de rock formada em Brasilia com letras marcantes e politicas",
+        "genre": "Rock",
+        "country": "Brasil",
+        "formed_year": 1982,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 7,
         "name": "Dua Lipa",
-        "bio": "Cantora britânica de pop conhecida por sucessos internacionais e estilo moderno"
+        "bio": "Cantora britanica de pop conhecida por sucessos internacionais e estilo moderno",
+        "genre": "Pop",
+        "country": "Reino Unido",
+        "formed_year": 2015,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 8,
         "name": "Marina Sena",
-        "bio": "Cantora brasileira de pop alternativo com sonoridade influenciada por música brasileira"
+        "bio": "Cantora brasileira de pop alternativo com sonoridade influenciada por musica brasileira",
+        "genre": "Pop",
+        "country": "Brasil",
+        "formed_year": 2021,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 9,
         "name": "Drake",
-        "bio": "Rapper e cantor canadense conhecido por misturar hip hop e R&B"
+        "bio": "Rapper e cantor canadense conhecido por misturar hip hop e RnB",
+        "genre": "Hip Hop",
+        "country": "Canada",
+        "formed_year": 2009,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 10,
         "name": "Lady Gaga",
-        "bio": "Cantora e atriz americana conhecida por performances ousadas e sucessos no pop"
+        "bio": "Cantora e atriz americana conhecida por performances ousadas e sucessos no pop",
+        "genre": "Pop",
+        "country": "EUA",
+        "formed_year": 2008,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 11,
         "name": "Arctic Monkeys",
-        "bio": "Banda britânica de indie rock formada em Sheffield"
+        "bio": "Banda britanica de indie rock formada em Sheffield",
+        "genre": "Rock",
+        "country": "Reino Unido",
+        "formed_year": 2002,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 12,
         "name": "Paramore",
-        "bio": "Banda americana de pop punk liderada por Hayley Williams"
+        "bio": "Banda americana de pop punk liderada por Hayley Williams",
+        "genre": "Pop Punk",
+        "country": "EUA",
+        "formed_year": 2004,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 13,
         "name": "Bad Bunny",
-        "bio": "Cantor porto riquenho de reggaeton e trap latino com grande impacto global"
+        "bio": "Cantor porto riquenho de reggaeton e trap latino com grande impacto global",
+        "genre": "Reggaeton",
+        "country": "Porto Rico",
+        "formed_year": 2016,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 14,
         "name": "Evanescence",
-        "bio": "Banda americana de rock com influencias de metal e vocal feminino marcante"
+        "bio": "Banda americana de rock com influencias de metal e vocal feminino marcante",
+        "genre": "Rock",
+        "country": "EUA",
+        "formed_year": 1995,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 15,
         "name": "Travis Scott",
-        "bio": "Rapper e produtor americano conhecido por estilo experimental no hip hop"
+        "bio": "Rapper e produtor americano conhecido por estilo experimental no hip hop",
+        "genre": "Hip Hop",
+        "country": "EUA",
+        "formed_year": 2013,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 16,
         "name": "Charlie Brown Jr",
-        "bio": "Banda brasileira de rock que mistura skate punk rap e reggae"
+        "bio": "Banda brasileira de rock que mistura skate punk rap e reggae",
+        "genre": "Rock",
+        "country": "Brasil",
+        "formed_year": 1992,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 17,
         "name": "Ivete Sangalo",
-        "bio": "Cantora brasileira de axé e musica pop com forte presença no carnaval"
+        "bio": "Cantora brasileira de axe e musica pop com forte presenca no carnaval",
+        "genre": "Axe",
+        "country": "Brasil",
+        "formed_year": 1993,
+        "image_url": "https://link-da-imagem.jpg"
     },
     {
         "id_artista": 18,
         "name": "My Chemical Romance",
-        "bio": "Banda americana de rock associada ao movimento emo dos anos 2000"
+        "bio": "Banda americana de rock associada ao movimento emo dos anos 2000",
+        "genre": "Pop Punk",
+        "country": "EUA",
+        "formed_year": 2001,
+        "image_url": "https://link-da-imagem.jpg"
     }
 ])
 
 
-# 3. Povoamento de Álbuns (com a nova Descrição e IDs)
+
+# 3. Povoamento de Álbuns (com genero adicionado)
 db.albums.insert_many([
+
     {
         "id_album": 1,
         "title": "Three Cheers for Sweet Revenge",
         "id_artista": 18,
         "year": 2004,
+        "genre": "Emo-Pop",
         "description": "Album que consolidou o som emo do grupo",
         "image": "https://link-da-imagem.jpg"
     },
@@ -122,6 +197,7 @@ db.albums.insert_many([
         "title": "The Black Parade",
         "id_artista": 18,
         "year": 2006,
+        "genre": "Rock Opera",
         "description": "Album conceitual que marcou a carreira da banda",
         "image": "https://link-da-imagem.jpg"
     },
@@ -131,6 +207,7 @@ db.albums.insert_many([
         "title": "Transpiracao Continua Prolongada",
         "id_artista": 16,
         "year": 1997,
+        "genre": "Skate Punk",
         "description": "Album de estreia com forte influencia do skate punk",
         "image": "https://link-da-imagem.jpg"
     },
@@ -140,6 +217,7 @@ db.albums.insert_many([
         "title": "Preco Curto Prazo Longo",
         "id_artista": 16,
         "year": 1999,
+        "genre": "Skate Punk",
         "description": "Album que ampliou o sucesso nacional da banda",
         "image": "https://link-da-imagem.jpg"
     },
@@ -149,16 +227,18 @@ db.albums.insert_many([
         "title": "Nadando com os Tubaroes",
         "id_artista": 16,
         "year": 2000,
+        "genre": "Skate Punk",
         "description": "Album com letras intensas e sonoridade marcante",
         "image": "https://link-da-imagem.jpg"
     },
 
     {
         "id_album": 6,
-        "title": "100 Porcento Charlie Brown Jr Abalando a Sua Fabrica",
+        "title": "Acustico MTV Charlie Brown Jr",
         "id_artista": 16,
-        "year": 2001,
-        "description": "Album que reforcou o estilo irreverente da banda",
+        "year": 2003,
+        "genre": "Pop Rock",
+        "description": "Album ao vivo em formato acustico com grandes sucessos da banda",
         "image": "https://link-da-imagem.jpg"
     },
 
@@ -167,6 +247,7 @@ db.albums.insert_many([
         "title": "Bocas Ordinarias",
         "id_artista": 16,
         "year": 2002,
+        "genre": "Skate Punk",
         "description": "Album com sucessos populares e letras reflexivas",
         "image": "https://link-da-imagem.jpg"
     },
@@ -176,6 +257,7 @@ db.albums.insert_many([
         "title": "Imunidade Musical",
         "id_artista": 16,
         "year": 2005,
+        "genre": "Rap Rock",
         "description": "Album premiado com forte impacto no rock nacional",
         "image": "https://link-da-imagem.jpg"
     },
@@ -185,6 +267,7 @@ db.albums.insert_many([
         "title": "All We Know Is Falling",
         "id_artista": 12,
         "year": 2005,
+        "genre": "Pop Punk",
         "description": "Album de estreia com sonoridade emo pop",
         "image": "https://link-da-imagem.jpg"
     },
@@ -194,15 +277,117 @@ db.albums.insert_many([
         "title": "RIOT",
         "id_artista": 12,
         "year": 2007,
+        "genre": "Pop Punk",
         "description": "Album que levou a banda ao sucesso mundial",
         "image": "https://link-da-imagem.jpg"
     },
 
     {
+        "id_album": 11,
+        "title": "brand new eyes",
+        "id_artista": 12,
+        "year": 2009,
+        "genre": "Alternative Rock",
+        "description": "Album com evolucao sonora e amadurecimento lirico",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+    {
+        "id_album": 12,
+        "title": "Paramore",
+        "id_artista": 12,
+        "year": 2013,
+        "genre": "Pop Rock",
+        "description": "Album com estilo mais pop e experimental",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+    {
+        "id_album": 13,
+        "title": "After Laughter",
+        "id_artista": 12,
+        "year": 2017,
+        "genre": "Pop Rock",
+        "description": "Album com sonoridade mais leve e influencias dos anos 80",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+    {
+        "id_album": 14,
+        "title": "This Is Why",
+        "id_artista": 12,
+        "year": 2023,
+        "genre": "Alternative Rock",
+        "description": "Album com abordagem mais madura e alternativa",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+    {
+        "id_album": 15,
+        "title": "WHEN WE ALL FALL ASLEEP WHERE DO WE GO",
+        "id_artista": 2,
+        "year": 2019,
+        "genre": "Alternative Pop",
+        "description": "Album de estreia com atmosfera sombria e moderna",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+    {
+        "id_album": 16,
+        "title": "Happier Than Ever",
+        "id_artista": 2,
+        "year": 2021,
+        "genre": "Alternative Pop",
+        "description": "Album que mistura pop alternativo e vulnerabilidade",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+    {
+        "id_album": 17,
+        "title": "Fearless",
+        "id_artista": 3,
+        "year": 2008,
+        "genre": "Country",
+        "description": "Album country pop que consolidou sua carreira",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+    {
+        "id_album": 18,
+        "title": "Red",
+        "id_artista": 3,
+        "year": 2012,
+        "genre": "Pop",
+        "description": "Album que mistura country e pop com letras intensas",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+    {
+        "id_album": 19,
+        "title": "1989",
+        "id_artista": 3,
+        "year": 2014,
+        "genre": "Pop",
+        "description": "Album totalmente pop com grandes hits mundiais",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+    {
+        "id_album": 20,
+        "title": "reputation",
+        "id_artista": 3,
+        "year": 2017,
+        "genre": "Pop",
+        "description": "Album com sonoridade mais sombria e eletronica",
+        "image": "https://link-da-imagem.jpg"
+    },
+
+        {
         "id_album": 21,
         "title": "Lover",
         "id_artista": 3,
         "year": 2019,
+        "genre": "Pop",
         "description": "Album com atmosfera romantica e colorida",
         "image": "https://link-da-imagem.jpg"
     },
@@ -212,6 +397,7 @@ db.albums.insert_many([
         "title": "folklore",
         "id_artista": 3,
         "year": 2020,
+        "genre": "Folk",
         "description": "Album mais intimista com influencias indie",
         "image": "https://link-da-imagem.jpg"
     },
@@ -221,6 +407,7 @@ db.albums.insert_many([
         "title": "Future Nostalgia",
         "id_artista": 7,
         "year": 2020,
+        "genre": "Pop",
         "description": "Album pop com forte influencia disco e anos 80",
         "image": "https://link-da-imagem.jpg"
     },
@@ -230,6 +417,7 @@ db.albums.insert_many([
         "title": "Coisas Naturais",
         "id_artista": 8,
         "year": 2023,
+        "genre": "Pop",
         "description": "Album com sonoridade brasileira moderna e sensual",
         "image": "https://link-da-imagem.jpg"
     },
@@ -239,6 +427,7 @@ db.albums.insert_many([
         "title": "Take Care",
         "id_artista": 9,
         "year": 2011,
+        "genre": "Hip Hop",
         "description": "Album que consolidou Drake no rap mundial",
         "image": "https://link-da-imagem.jpg"
     },
@@ -248,6 +437,7 @@ db.albums.insert_many([
         "title": "Views",
         "id_artista": 9,
         "year": 2016,
+        "genre": "Hip Hop",
         "description": "Album com mistura de rap e dancehall",
         "image": "https://link-da-imagem.jpg"
     },
@@ -257,6 +447,7 @@ db.albums.insert_many([
         "title": "Scorpion",
         "id_artista": 9,
         "year": 2018,
+        "genre": "Hip Hop",
         "description": "Album duplo com sucessos comerciais globais",
         "image": "https://link-da-imagem.jpg"
     },
@@ -266,6 +457,7 @@ db.albums.insert_many([
         "title": "Dois",
         "id_artista": 6,
         "year": 1986,
+        "genre": "Post-Punk",
         "description": "Album com classicos do rock brasileiro",
         "image": "https://link-da-imagem.jpg"
     },
@@ -275,6 +467,7 @@ db.albums.insert_many([
         "title": "Que Pais E Esse",
         "id_artista": 6,
         "year": 1987,
+        "genre": "Post-Punk",
         "description": "Album com forte critica social e politica",
         "image": "https://link-da-imagem.jpg"
     },
@@ -284,15 +477,17 @@ db.albums.insert_many([
         "title": "Revolver",
         "id_artista": 4,
         "year": 1966,
+        "genre": "Rock",
         "description": "Album inovador que marcou a musica mundial",
         "image": "https://link-da-imagem.jpg"
     },
 
-        {
+    {
         "id_album": 31,
         "title": "Abbey Road",
         "id_artista": 4,
         "year": 1969,
+        "genre": "Rock",
         "description": "Album classico com producao sofisticada",
         "image": "https://link-da-imagem.jpg"
     },
@@ -302,6 +497,7 @@ db.albums.insert_many([
         "title": "Let It Be",
         "id_artista": 4,
         "year": 1970,
+        "genre": "Rock",
         "description": "Ultimo album lancado pela banda",
         "image": "https://link-da-imagem.jpg"
     },
@@ -311,6 +507,7 @@ db.albums.insert_many([
         "title": "The Fame",
         "id_artista": 10,
         "year": 2008,
+        "genre": "Pop",
         "description": "Album de estreia com grandes hits pop",
         "image": "https://link-da-imagem.jpg"
     },
@@ -320,6 +517,7 @@ db.albums.insert_many([
         "title": "The Fame Monster",
         "id_artista": 10,
         "year": 2009,
+        "genre": "Pop",
         "description": "Reedicao com sucessos marcantes do pop",
         "image": "https://link-da-imagem.jpg"
     },
@@ -329,6 +527,7 @@ db.albums.insert_many([
         "title": "Born This Way",
         "id_artista": 10,
         "year": 2011,
+        "genre": "Pop",
         "description": "Album com mensagem de auto aceitacao",
         "image": "https://link-da-imagem.jpg"
     },
@@ -338,7 +537,8 @@ db.albums.insert_many([
         "title": "ARTPOP",
         "id_artista": 10,
         "year": 2013,
-        "description": "Album experimental com influencias eletronicas",
+        "genre": "Pop",
+        "description": "Album ambicioso com influencias eletronicas",
         "image": "https://link-da-imagem.jpg"
     },
 
@@ -347,6 +547,7 @@ db.albums.insert_many([
         "title": "Humbug",
         "id_artista": 11,
         "year": 2009,
+        "genre": "Indie Rock",
         "description": "Album com sonoridade mais sombria e psicodelica",
         "image": "https://link-da-imagem.jpg"
     },
@@ -356,6 +557,7 @@ db.albums.insert_many([
         "title": "Suck It And See",
         "id_artista": 11,
         "year": 2011,
+        "genre": "Indie Rock",
         "description": "Album com estilo mais romantico e direto",
         "image": "https://link-da-imagem.jpg"
     },
@@ -365,11 +567,13 @@ db.albums.insert_many([
         "title": "AM",
         "id_artista": 11,
         "year": 2013,
+        "genre": "Indie Rock",
         "description": "Album com grande sucesso comercial da banda",
         "image": "https://link-da-imagem.jpg"
     }
 
 ])
+
 
 
 # 4. Povoamento de Usuário (Apenas para o Rafael testar o Profile)
